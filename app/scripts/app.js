2ac0cad395966bc3047d6bf33e70fac60549e3d9
YonasBerhe <yonas.berhe007@gmail.com>
2015-04-21 23:36:01 -0700

    Hi I'm a noob at angular and this is me learning

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angApp
+ * @description
+ * # angApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('controller', []);
+
+function (){
+
+	'use strict';
+
+	var alert;
+	
+	function Storecontroller()
+	{
+	alert('Welcome, Gregg');
+	}
+
+	addEventListener('load'StoreController);
+}
